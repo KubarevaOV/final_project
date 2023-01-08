5f866ed14bb2ebe471eab8940d75ef5577decf30
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2023-01-08 21:15:27 +0300

    Program code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+string [] wordsArray = new string[]{"Lada", "Kia", "Ford", "Yo", "Zaz", "Ferrari"};
+List<string> resultArray = new List<string>();
+
+for (int counter = 0; counter < wordsArray.Length; counter++) {
+    if (wordsArray[counter].Length <= 3) {
+        resultArray.Add(wordsArray[counter]);
+    }
+}
+
+String[] str = resultArray.ToArray();
+
+for (int i = 0; i < str.Length; i++) {
+    Console.Write("{0} ", str[i]);
+}
+Console.WriteLine();
